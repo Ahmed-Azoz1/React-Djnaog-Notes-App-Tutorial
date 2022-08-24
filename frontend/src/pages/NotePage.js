a404d1ceda1856651acd59a4670627fd1fcb5025
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2022-08-24 11:13:18 +0200

    Create Function DeleteNote In BackEnd And convert it to FrontEnd

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -2,7 +2,7 @@ import React , {useState,useEffect} from 'react'
 import { Link } from 'react-router-dom';
 import { ReactComponent as ArrowLeft } from '../assets/chevron-left.svg'
 
-const NotePage = ({ match }) => {
+const NotePage = ({ match , history }) => {
     let noteId = match.params.id;
 
     let [note , setNote] = useState(null)
@@ -12,21 +12,47 @@ const NotePage = ({ match }) => {
     },[noteId])
 
     let getNote = async () =>{
-        let response = await fetch(`/api/notes/${noteId}`)
+        let response = await fetch(`/api/notes/${noteId}/`)
         let data = await response.json()
         setNote(data)
+    }
+
+    let updateNote = async () =>{
+        fetch(`/api/notes/${noteId}/update/`,
+            {
+                method:"PUT",
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify(note)
+            }
+        )
+    }
+
+    let deleteNote = async () =>{
+        fetch(`/api/notes/${noteId}/delete/`,{
+            method:"DELETE",
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        history.push('/')
+    }
+
+    let handleSubmit = () => {
+        updateNote()
+        history.push('/')
     }
 
     return (
         <div className='note'>
             <div className='note-header'>
                 <h3>
-                    <Link to="/">
-                        <ArrowLeft />
-                    </Link>
+                    <ArrowLeft onClick={handleSubmit} />
                 </h3>
+                <button onClick={deleteNote}>Delete</button>
             </div>
-            <textarea defaultValue={note?.body}></textarea>
+            <textarea onChange={(e) =>{setNote({...note,'body':e.target.value})}} defaultValue={note?.body}></textarea>
         </div>
     )
 }
