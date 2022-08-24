8e84c545ece2ed25b125d1afafac2d3c5c5062c0
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2022-08-24 17:24:39 +0200

    End Project But We Have A Problem Delay response data from Backend to Frontend

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Note
 from .serializers import NoteSerializer
+from .utils import *
 # Create your views here.
 @api_view(['GET'])
 def getRoutes(request):
@@ -43,42 +44,51 @@ def getRoutes(request):
     return Response(routes)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def getNotes(request):
-    notes = Note.objects.all().order_by('-updated')
-    serializer = NoteSerializer(notes,many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        return getNotesList(request)
 
+    if request.method == 'POST':
+        return createNote(request)
 
-@api_view(['GET'])
-def getNote(request,pk):
-    notes = Note.objects.get(id=pk)
-    serializer = NoteSerializer(notes,many=False)
-    return Response(serializer.data)
+@api_view(['GET', 'PUT', 'DELETE'])
+def getNote(request, pk):
+    if request.method == 'GET':
+        return getNoteDetail(request, pk)
 
-@api_view(['POST'])
-def createNote(request):
-    data = request.data
-    note = Note.objects.create(
-        body = data['body']
-    )
-    serializer = NoteSerializer(note,many=False)
-    return Response(serializer.data)
+    if request.method == 'PUT':
+        return updateNote(request, pk)
 
-@api_view(['PUT'])
-def updateNote(request,pk):
-    data = request.data
-    note = Note.objects.get(id=pk)
-    serializer = NoteSerializer(instance=note,data=data)
-    
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+    if request.method == 'DELETE':
+        return deleteNote(request, pk)
 
 
 
-@api_view(['DELETE'])
-def deleteNote(request,pk):
-    note = Note.objects.get(id=pk)
-    note.delete()
-    return Response('Note Was Deleted')
+
+# @api_view(['POST'])
+# def createNote(request):
+#     data = request.data
+#     note = Note.objects.create(
+#         body = data['body']
+#     )
+#     serializer = NoteSerializer(note,many=False)
+#     return Response(serializer.data)
+
+
+# @api_view(['PUT'])
+# def updateNote(request,pk):
+#     data = request.data
+#     note = Note.objects.get(id=pk)
+#     serializer = NoteSerializer(instance=note,data=data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+
+
+# @api_view(['DELETE'])
+# def deleteNote(request,pk):
+#     note = Note.objects.get(id=pk)
+#     note.delete()
+#     return Response('Note Was Deleted')
--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,68 +1,68 @@
-import React , {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { ReactComponent as ArrowLeft } from '../assets/chevron-left.svg'
 
-const NotePage = ({ match , history }) => {
-    let noteId = match.params.id;
-
-    let [note , setNote] = useState(null)
-
-    useEffect(()=>{
+const NotePage = ({ match, history }) => {
+    let noteId = match.params.id
+    let [note, setNote] = useState(null)
+    useEffect(() => {
         getNote()
     }, [noteId])
 
-    let getNote = async () =>{
-        if(noteId === 'new') return
+
+    let getNote = async () => {
+        if (noteId === 'new') return
+
         let response = await fetch(`/api/notes/${noteId}/`)
         let data = await response.json()
         setNote(data)
     }
 
-    let createNote = async () =>{
-        fetch(`/api/notes/create/`,{
+    let createNote = async () => {
+        fetch(`/api/notes/`, {
             method: "POST",
             headers: {
-                'Content-Type':'application/json'
+                'Content-Type': 'application/json'
             },
-            body:JSON.stringify(note)
+            body: JSON.stringify(note)
         })
     }
 
-    let updateNote = async () =>{
-        fetch(`/api/notes/${noteId}/update/`,
-            {
-                method:"PUT",
-                headers:{
-                    'Content-Type':'application/json'
-                },
-                body:JSON.stringify(note)
-            }
-        )
+
+    let updateNote = async () => {
+        fetch(`/api/notes/${noteId}/`, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(note)
+        })
     }
 
-    let deleteNote = async () =>{
-        fetch(`/api/notes/${noteId}/delete/`,{
-            method:"DELETE",
-            'headers':{
-                'Content-Type':'application/json'
+
+    let deleteNote = async () => {
+        fetch(`/api/notes/${noteId}/`, {
+            method: 'DELETE',
+            'headers': {
+                'Content-Type': 'application/json'
             }
         })
         history.push('/')
     }
 
     let handleSubmit = () => {
-        if(noteId !== 'new' && note.body === ''){
+        console.log('NOTE:', note)
+        if (noteId !== 'new' && note.body === '') {
             deleteNote()
-        }else if(noteId !== 'new'){
+        } else if (noteId !== 'new') {
             updateNote()
-        }else if(noteId === 'new' && note.body !== null){
+        } else if (noteId === 'new' && note.body !== null) {
             createNote()
         }
         history.push('/')
     }
 
-    let handleChange = (value) =>{
-        setNote(note =>({...note,'body':value}))
-        console.log('handle change:',note)
+    let handleChange = (value) => {
+        setNote(note => ({ ...note, 'body': value }))
     }
 
     return (
